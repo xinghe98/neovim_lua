8a01114d70d7d93cf5bec7b40c715633207cbf0f
xinghe98 <47097088+xinghe98@users.noreply.github.com>
2023-02-04 13:25:33 +0800

    Update options.lua

--- lua/options.lua
+++ lua/options.lua
@@ -31,7 +31,7 @@ vim.o.splitbelow = true
 vim.o.pumheight = 10
 vim.o.splitright = true
 vim.opt.signcolumn = "yes"
-vim.notify = require('notify')
+-- vim.notify = require('notify')
 vim.cmd([[
 if has("autocmd")
   au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
