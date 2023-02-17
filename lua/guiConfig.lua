f2e008d34482011ef0580cb99a144b48e1ea2180
xinghe98 <lcg9899@gmail.com>
2023-02-17 17:40:13 +0800

    u

--- lua/guiConfig.lua
+++ lua/guiConfig.lua
@@ -1,8 +1,5 @@
 vim.cmd([[
 if exists("g:neovide")
 	set guifont=Consolas_NF:h16
-	let g:neovide_transparency = 0.8
-	let g:transparency = 0.8
-	let g:neovide_background_color = '#0f1117'.printf('%x', float2nr(255 * g:transparency))
 endif
 ]])
