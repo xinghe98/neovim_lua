6f505bceca8c25effa0c5b5bcb8180373c050620
xinghe98 <lcg9899@gmail.com>
2023-02-18 10:44:36 +0800

    h

--- lua/packsettings/bufferline.lua
+++ lua/packsettings/bufferline.lua
@@ -1,30 +1,30 @@
 -- bfferline 配置
 -- https://github.com/akinsho/bufferline.nvim#configuration
 require("bufferline").setup({
-  options = {
-    -- 关闭 Tab 的命令
-    close_command = "Bdelete! %d",
-    right_mouse_command = "Bdelete! %d",
-    -- 侧边栏配置
-    -- 左侧让出 nvim-tree 的位置，显示文字 File Explorer
-    offsets = {
-      {
-        filetype = "NvimTree",
-        text = "File Explorer",
-        highlight = "Directory",
-        text_align = "left",
-      },
-    },
-    diagnostics = "coc",
-    -- 可选，显示 LSP 报错图标
-    ---@diagnostic disable-next-line: unused-local
-    diagnostics_indicator = function(count, level, diagnostics_dict, context)
-      local s = " "
-      for e, n in pairs(diagnostics_dict) do
-        local sym = e == "error" and " " or (e == "warning" and " " or "")
-        s = s .. n .. sym
-      end
-      return s
-    end,
-  },
+	options = {
+		mode = "buffers",
+		-- 显示id
+		numbers = "ordinal",
+		-- 侧边栏配置
+		-- 左侧让出 nvim-tree 的位置，显示文字 File Explorer
+		offsets = {
+			{
+				filetype = "NvimTree",
+				text = "File Explorer",
+				highlight = "Directory",
+				text_align = "left",
+			},
+		},
+		diagnostics = "coc",
+		-- 可选，显示 LSP 报错图标
+		---@diagnostic disable-next-line: unused-local
+		diagnostics_indicator = function(count, level, diagnostics_dict, context)
+			local s = " "
+			for e, n in pairs(diagnostics_dict) do
+				local sym = e == "error" and " " or (e == "warning" and " " or "")
+				s = s .. n .. sym
+			end
+			return s
+		end,
+	},
 })
