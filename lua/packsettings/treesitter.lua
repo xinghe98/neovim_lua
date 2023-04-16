2b44f470af5c4ff029ea9cb41880293f3d69f503
xinghe98 <lcg9899@gmail.com>
2023-04-16 08:43:32 +0000

    done

--- lua/packsettings/treesitter.lua
+++ lua/packsettings/treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup {
-	ensure_installed = { "go", "python", "typescript", "lua" },
+	ensure_installed = { "go", "python", "typescript", "lua","markdown" },
 	highlight = {
 		-- ...
 
