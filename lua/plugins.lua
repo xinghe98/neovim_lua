1c9697de97b07c4cfdc8bef0ee547879788b7ca9
xinghe98 <lcg9899@gmail.com>
2023-08-29 11:21:45 +0800

    x

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -16,37 +16,37 @@ require("lazy").setup({
 	{
 		"williamboman/mason.nvim",
 	},
-	{"williamboman/mason-lspconfig.nvim"},
-{
+	{ "williamboman/mason-lspconfig.nvim" },
+	{
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 		config = function()
 			require("lsp/mason")
 		end,
 	},
-	{"neovim/nvim-lspconfig"},
+	{ "neovim/nvim-lspconfig" },
 	{
 		"hrsh7th/nvim-cmp",
 		config = function()
 			require("lsp.cmp")
 		end,
 	},
-	{"hrsh7th/cmp-nvim-lsp"},
-	{"saadparwaiz1/cmp_luasnip"}, -- Snippets source for nvim-cmp
-	{"rafamadriz/friendly-snippets"}, -- Snippets (vscode-like) collection for different languages
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "saadparwaiz1/cmp_luasnip" }, -- Snippets source for nvim-cmp
+	{ "rafamadriz/friendly-snippets" }, -- Snippets (vscode-like) collection for different languages
 	{
 		"L3MON4D3/LuaSnip", -- Snippets plugin
 		config = function()
 			require("luasnip.loaders.from_vscode").lazy_load()
 		end,
 	},
-	{"ray-x/lsp_signature.nvim"},
+	{ "ray-x/lsp_signature.nvim" },
 	-- use 'hrsh7th/cmp-nvim-lsp-signature-help'
-	{"hrsh7th/cmp-buffer"},
-	{"hrsh7th/cmp-path"},
-	{"hrsh7th/cmp-nvim-lua"},
-	{"hrsh7th/cmp-look"},
-	{"f3fora/cmp-spell"},
-	{"hrsh7th/cmp-cmdline"},
+	{ "hrsh7th/cmp-buffer" },
+	{ "hrsh7th/cmp-path" },
+	{ "hrsh7th/cmp-nvim-lua" },
+	{ "hrsh7th/cmp-look" },
+	{ "f3fora/cmp-spell" },
+	{ "hrsh7th/cmp-cmdline" },
 	{
 		"nvimdev/lspsaga.nvim",
 		after = "nvim-lspconfig",
@@ -59,20 +59,25 @@ require("lazy").setup({
 			{ "nvim-treesitter/nvim-treesitter" },
 		},
 	},
-{
+	{
 		"stevearc/conform.nvim",
 		config = function()
 			require("packsettings.conform")
 		end,
 	},
+	{
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	},
 	--------lsp end -------------------
-	{"rcarriga/nvim-notify"},
-	{"folke/todo-comments.nvim"},
-	{"ap/vim-css-color"},
-	{"theniceboy/nvim-deus"},
-	{"marko-cerovac/material.nvim"},
-	{"rmehri01/onenord.nvim"},
-	{"folke/tokyonight.nvim"},
-	{"hardhackerlabs/theme-vim"},
-
+	{ "rcarriga/nvim-notify" },
+	{ "folke/todo-comments.nvim" },
+	{ "ap/vim-css-color" },
+	{ "theniceboy/nvim-deus" },
+	{ "marko-cerovac/material.nvim" },
+	{ "rmehri01/onenord.nvim" },
+	{ "folke/tokyonight.nvim" },
+	{ "hardhackerlabs/theme-vim" },
 })
