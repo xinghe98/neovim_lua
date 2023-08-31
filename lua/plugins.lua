3aae9dcd4f694543774eae26d4f971201ab3cf67
xinghe98 <lcg9899@gmail.com>
2023-08-31 12:58:54 +0800

    x

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
 	{ "williamboman/mason-lspconfig.nvim" },
 	{
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		lazy = false,
 		config = function()
 			require("lsp/mason")
 		end,
@@ -35,25 +36,26 @@ require("lazy").setup({
 			{ "f3fora/cmp-spell" },
 			-- { "hrsh7th/cmp-cmdline" },
 			{ "hrsh7th/cmp-calc" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "saadparwaiz1/cmp_luasnip" }, -- Snippets source for nvim-cmp
+			{ "rafamadriz/friendly-snippets" }, -- Snippets (vscode-like) collection for different languages
+			{
+				"L3MON4D3/LuaSnip", -- Snippets plugin
+				config = function()
+					require("luasnip.loaders.from_vscode").lazy_load()
+				end,
+			},
+			{ "ray-x/lsp_signature.nvim" },
+			-- use 'hrsh7th/cmp-nvim-lsp-signature-help'
 		},
 		config = function()
 			require("lsp.cmp")
 		end,
 	},
-	{ "hrsh7th/cmp-nvim-lsp" },
-	{ "saadparwaiz1/cmp_luasnip" }, -- Snippets source for nvim-cmp
-	{ "rafamadriz/friendly-snippets" }, -- Snippets (vscode-like) collection for different languages
-	{
-		"L3MON4D3/LuaSnip", -- Snippets plugin
-		config = function()
-			require("luasnip.loaders.from_vscode").lazy_load()
-		end,
-	},
-	{ "ray-x/lsp_signature.nvim" },
-	-- use 'hrsh7th/cmp-nvim-lsp-signature-help'
 	{
 		"nvimdev/lspsaga.nvim",
 		-- after = "nvim-lspconfig",
+		event = "VimEnter",
 		config = function()
 			require("lspsaga").setup({})
 		end,
@@ -61,19 +63,19 @@ require("lazy").setup({
 			{ "nvim-tree/nvim-web-devicons" },
 			--Please make sure you install markdown and markdown_inline parser
 			{ "nvim-treesitter/nvim-treesitter" },
+			{
+				"stevearc/conform.nvim",
+				config = function()
+					require("packsettings.conform")
+				end,
+			},
+			{
+				"windwp/nvim-autopairs",
+				config = function()
+					require("nvim-autopairs").setup({})
+				end,
+			},
 		},
-	},
-	{
-		"stevearc/conform.nvim",
-		config = function()
-			require("packsettings.conform")
-		end,
-	},
-	{
-		"windwp/nvim-autopairs",
-		config = function()
-			require("nvim-autopairs").setup({})
-		end,
 	},
 	--------lsp end -------------------
 	{
@@ -85,23 +87,25 @@ require("lazy").setup({
 	},
 	{
 		"liuchengxu/vista.vim",
-		lazy = false,
+		event = "BufEnter",
 		config = function()
 			require("packsettings.vista")
 		end,
 	},
-	{ "mg979/vim-visual-multi", branch = "master" },
-	{ "posva/vim-vue" },
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			require("packsettings.treesitter")
-		end,
-	},
+	{ "mg979/vim-visual-multi", branch = "master", lazy = false },
 	{
 		"HiPhish/nvim-ts-rainbow2",
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		event = "VimEnter",
+		dependencies = {
+			{
+				"nvim-treesitter/nvim-treesitter",
+				event = "VimEnter",
+				build = ":TSUpdate",
+				config = function()
+					require("packsettings.treesitter")
+				end,
+			},
+		},
 		config = function()
 			require("packsettings.rainbow")
 		end,
@@ -132,7 +136,7 @@ require("lazy").setup({
 			require("packsettings.lualine")
 		end,
 	},
-	{ "voldikss/vim-translator" },
+	{ "voldikss/vim-translator", event = "BufEnter" },
 	{
 		"tpope/vim-surround",
 		version = "*", -- Use for stability; omit to use `main` branch for the latest features
@@ -140,13 +144,15 @@ require("lazy").setup({
 	},
 	{
 		"lukas-reineke/indent-blankline.nvim",
+		event = "BufEnter",
 		config = function()
 			require("packsettings.indentline")
 		end,
 	},
-	{ "xiyaowong/nvim-cursorword" },
+	{ "xiyaowong/nvim-cursorword", lazy = false },
 	{
 		"akinsho/bufferline.nvim",
+		event = "BufNew",
 		version = "v3.*",
 		config = function()
 			require("packsettings.bufferline")
@@ -155,18 +161,18 @@ require("lazy").setup({
 	-- 快速注释
 	{
 		"b3nj5m1n/kommentary",
-		event = "ColorScheme",
+		event = "VeryLazy",
 		config = function()
 			require("packsettings.kommentary")
 		end,
 	},
 	-- 终端悬浮窗
-	{ "voldikss/vim-floaterm" },
+	{ "voldikss/vim-floaterm", lazy = false },
 	-- 文件搜索插件
 	{
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.2",
-		event = "ColorScheme",
+		event = "VimEnter",
 		dependencies = {
 			"nvim-lua/plenary.nvim", -- Lua 开发模块
 			"BurntSushi/ripgrep", -- 文字查找
@@ -179,20 +185,26 @@ require("lazy").setup({
 	-- Markdown
 	{
 		"iamcco/markdown-preview.nvim",
+		lazy = false,
 		config = function()
 			require("packsettings.markdownpre")
 		end,
-	},
-	{ "dhruvasagar/vim-table-mode" },
-	{ "mzlogin/vim-markdown-toc" },
-	{
-		"dkarter/bullets.vim",
-		config = function()
-			require("packsettings.bullets")
-		end,
+		dependencies = {
+
+			{ "dhruvasagar/vim-table-mode" },
+			{ "mzlogin/vim-markdown-toc" },
+			{ "godlygeek/tabular" },
+			{
+				"dkarter/bullets.vim",
+				config = function()
+					require("packsettings.bullets")
+				end,
+			},
+		},
 	},
 	{
 		"fatih/vim-go",
+		lazy = false,
 		build = ":GoUpdateBinaries",
 		config = function()
 			require("packsettings.vimgo")
@@ -201,18 +213,17 @@ require("lazy").setup({
 	{
 		"github/copilot.vim",
 		lazy = false,
-		event = "ColorScheme",
+		event = "VimEnter",
 		config = function()
 			vim.g.copilot_enabled = true
 			vim.g.copilot_no_tab_map = true
 			vim.g.copilot_no_cr_map = true
 		end,
 	},
-	{ "godlygeek/tabular" },
 	--------一些美化啥的 -------------------
 	{ "rcarriga/nvim-notify" },
 	{ "folke/todo-comments.nvim" },
-	{ "ap/vim-css-color" },
+	{ "ap/vim-css-color", event = "BufWinEnter" },
 	{ "theniceboy/nvim-deus", lazy = false, priority = 1000 },
 	{ "marko-cerovac/material.nvim", lazy = false, priority = 1000 },
 	{ "rmehri01/onenord.nvim", lazy = false, priority = 1000 },
