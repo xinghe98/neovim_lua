d445b360ca41939502444098000117f872eca461
xinghe98 <lcg9899@gmail.com>
2023-09-21 07:34:44 +0800

    bug

--- lua/packsettings/coc.lua
+++ lua/packsettings/coc.lua
@@ -8,7 +8,7 @@ vim.opt.writebackup = false
 
 -- Having longer updatetime (default is 4000 ms = 4s) leads to noticeable
 -- delays and poor user experience
-vim.opt.updatetime = 300
+vim.opt.updatetime = 100
 
 -- Always show the signcolumn, otherwise it would shift the text each time
 -- diagnostics appeared/became resolved
