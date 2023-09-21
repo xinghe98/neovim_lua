2caf653095158b299ae431dc4be993de6ca0cf92
xinghe98 <lcg9899@gmail.com>
2023-09-21 18:32:27 +0800

    todocomments keymap

--- lua/packsettings/treesitter.lua
+++ lua/packsettings/treesitter.lua
@@ -1,5 +1,6 @@
 require("nvim-treesitter.configs").setup {
-	ensure_installed = { "go", "python", "typescript", "lua", "markdown", "markdown_inline", "query", "c", "vim" },
+	ensure_installed = { "go", "python", "typescript", "lua", "markdown", "markdown_inline", "query", "c", "vim", "html",
+		"vue" },
 	highlight = {
 		enable = true,
 	},
