4e7cfeb9eda269161474dccc02b318cf0e0f2207
李星河 <lixinghe@lixinghedeiMac-Pro.local>
2023-11-07 20:18:56 +0800

    x

--- lua/packsettings/telescope.lua
+++ lua/packsettings/telescope.lua
@@ -4,6 +4,18 @@ vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 require("telescope").setup({
+	defaults = {
+		file_ignore_patterns = {
+			"%.env",
+			"yarn.lock",
+			"package-lock.json",
+			"lazy-lock.json",
+			"init.sql",
+			"target/.*",
+			".git/.*", "node_modules",
+			"dist"
+		}
+	},
 	extensions = {
 		coc = {
 			theme = 'dropdown',
