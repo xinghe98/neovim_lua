f6a2bc30f617450f7980b0265237a144614f1784
xinghe98 <47097088+xinghe98@users.noreply.github.com>
2024-06-02 13:20:34 +0800

    Update treesitter.lua

--- lua/packsettings/treesitter.lua
+++ lua/packsettings/treesitter.lua
@@ -1,4 +1,5 @@
 require("nvim-treesitter.configs").setup {
+	ignore_install = { "help" },
 	ensure_installed = { "go", "python", "typescript", "markdown", "markdown_inline", "query", "c", "vim", "html", "vue" },
 	highlight = {
 		enable = true,
