20269e6bf19f375b143d47ea8c4c7515cd897c5c
李星河 <lixinghe@lixinghedeiMac-Pro.local>
2024-07-20 20:09:32 +0800

    u

--- lua/guiConfig.lua
+++ lua/guiConfig.lua
@@ -1,8 +1,14 @@
-vim.cmd([[
-if exists("g:neovide")
-	set guifont=Monaco_Nerd_font:h13
-	let g:neovide_transparency = 0.9
-	let g:transparency = 0.9
-	let g:neovide_background_color = '#0f1117'.printf('%x', float2nr(255 * g:transparency))
-endif
-]])
+if vim.g.neovide then
+	-- Helper function for transparency formatting
+	local alpha = function()
+		return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
+	end
+	-- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
+	vim.g.neovide_remember_window_size = true
+	vim.g.neovide_transparency = 0.5
+	vim.g.transparency = 0.5
+	-- vim.g.neovide_background_color = "#0f1117" .. alpha()
+	vim.o.guifont = "Monaco_Nerd_Font:h19"
+	vim.g.neovide_window_blurred = true
+	-- Put anything you want to happen only in Neovide here
+end
