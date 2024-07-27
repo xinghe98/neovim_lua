84d7d6c7ff93c73d0573816a835627c982410b14
李星河 <lixinghe@lixinghedeiMac-Pro.local>
2024-07-27 18:39:01 +0800

    u

--- lua/packsettings/rainbow.lua
+++ lua/packsettings/rainbow.lua
@@ -1,11 +1,11 @@
-require('nvim-treesitter.configs').setup {
+require("nvim-treesitter.configs").setup({
 	rainbow = {
 		enable = true,
 		-- list of languages you want to disable the plugin for
-		disable = { 'jsx', 'cpp' },
+		-- disable = { 'jsx', 'cpp' },
 		-- Which query to use for finding delimiters
-		query = 'rainbow-parens',
+		query = "rainbow-parens",
 		-- Highlight the entire buffer all at once
-		strategy = require('ts-rainbow').strategy.global,
-	}
-}
+		strategy = require("ts-rainbow").strategy.global,
+	},
+})
