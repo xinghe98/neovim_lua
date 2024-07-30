c64dcec514eb6336625434b47c61d68316c5c7bf
Your Name <lcg9899@gmail.com>
2024-07-30 12:21:32 +0800

    build

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -104,7 +104,6 @@ require("lazy").setup({
 	},
 	{
 		"akinsho/flutter-tools.nvim",
-		lazy = false,
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"stevearc/dressing.nvim", -- optional for vim.ui.select
