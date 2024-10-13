f7cac7ef2dcef3b42e13fb7dac2042c7574c59ea
xinghe98 <lcg9899@gmail.com>
2024-10-13 20:23:31 +0800

    s

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,3 +1,15 @@
+require("onedarkpro").setup({
+	styles = {
+		comments = "bold",
+		properties = "bold",
+		functions = "bold",
+		keywords = "italic",
+		operators = "bold",
+		conditionals = "italic",
+		loops = "italic",
+		booleans = "bold,italic",
+	},
+})
 vim.cmd("colorscheme onedark")
 -- vim.cmd("colorscheme material")
 -- vim.cmd("colorscheme hardhacker")
