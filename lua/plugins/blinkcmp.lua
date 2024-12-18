5e816154aa27510d947a4539ebf15c663f230fbb
xinghe98 <lcg9899@gmail.com>
2024-12-18 21:57:46 +0800

    break change

--- lua/plugins/blinkcmp.lua
+++ lua/plugins/blinkcmp.lua
@@ -56,6 +56,7 @@ return {
       ["<S-Tab>"] = { "snippet_backward", "select_prev", "fallback" },
       ["<Up>"] = { "snippet_backward", "select_prev" },
       ["<C-p>"] = { "snippet_backward", "select_prev" },
+      ["<ESC>"] = { "cancel", "fallback" },
     },
     appearance = {
       -- Sets the fallback highlight groups to nvim-cmp's highlight groups
@@ -100,6 +101,29 @@ return {
             menu_north = { "w", "e", "n", "s" },
             menu_south = { "w", "e", "s", "n" },
           },
+        },
+      },
+      list = {
+        -- Maximum number of items to display
+        max_items = 200,
+        -- Controls if completion items will be selected automatically,
+        -- and whether selection automatically inserts
+        selection = "auto_insert",
+        -- Controls how the completion items are selected
+        -- 'preselect' will automatically select the first item in the completion list
+        -- 'manual' will not select any item by default
+        -- 'auto_insert' will not select any item by default, and insert the completion items automatically
+        -- when selecting them
+        --
+        -- You may want to bind a key to the `cancel` command, which will undo the selection
+        -- when using 'auto_insert'
+        cycle = {
+          -- When `true`, calling `select_next` at the *bottom* of the completion list
+          -- will select the *first* completion item.
+          from_bottom = true,
+          -- When `true`, calling `select_prev` at the *top* of the completion list
+          -- will select the *last* completion item.
+          from_top = true,
         },
       },
       menu = {
