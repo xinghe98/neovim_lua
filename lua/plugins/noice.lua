f154e4e31dba9c807d9d9e645d5e5e0059c2fd55
xinghe98 <lcg9899@gmail.com>
2025-03-02 19:25:57 +0800

    u

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -41,11 +41,11 @@ return {
         -- NOTE: If you enable messages, then the cmdline is enabled automatically.
         -- This is a current Neovim limitation.
         enabled = true, -- enables the Noice messages UI
-        view = "mini", -- default view for messages
-        view_error = "notify", -- view for errors
-        view_warn = "mini", -- view for warnings
-        view_history = "messages", -- view for :messages
-        view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+        -- view = "mini", -- default view for messages
+        -- view_error = "notify", -- view for errors
+        -- view_warn = "mini", -- view for warnings
+        -- view_history = "messages", -- view for :messages
+        -- view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
       },
       lsp = {
         hover = {
