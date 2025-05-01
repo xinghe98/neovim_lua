345b73abebbc265f63e346a053e92c1701b0623a
xinghe98 <lcg9899@gmail.com>
2025-05-01 15:44:29 +0800

    add markdown

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,17 @@
+return {
+  "iamcco/markdown-preview.nvim",
+  ft = "markdown",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "cd app && yarn install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+  end,
+  dependencies = {
+    { "dhruvasagar/vim-table-mode" },
+    { "mzlogin/vim-markdown-toc" },
+    { "godlygeek/tabular" },
+    {
+      "dkarter/bullets.vim",
+    },
+  },
+}
