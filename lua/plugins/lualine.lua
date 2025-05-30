8d090a38f0b4b7a624a81a634e63da38b6061da6
xinghe98 <lcg9899@gmail.com>
2025-05-30 12:48:17 +0800

    u

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -89,9 +89,23 @@ return {
             },
             {
               function()
-                require("codeium.virtual_text").set_statusbar_refresh(function()
-                  require("lualine").refresh()
-                end)
+                local status = require("codeium.virtual_text").status()
+
+                if status.state == "idle" then
+                  -- Output was cleared, for example when leaving insert mode
+                  return "🐣"
+                end
+
+                if status.state == "waiting" then
+                  -- Waiting for response
+                  return "🐥"
+                end
+
+                if status.state == "completions" and status.total > 0 then
+                  return string.format("🐥 %d/%d", status.current, status.total)
+                end
+
+                return "⛔️"
               end,
             },
             {
