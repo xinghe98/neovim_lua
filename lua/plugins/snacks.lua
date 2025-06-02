7207a893468f1698d2258c98810a825e6415251c
xinghe98 <lcg9899@gmail.com>
2025-06-02 23:20:02 +0800

    pu

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,7 +6,27 @@ return {
         enabled = false,
       },
       explorer = { enabled = false },
-      indent = { enabled = true },
+      indent = {
+        enabled = true,
+        chunk = {
+          -- when enabled, scopes will be rendered as chunks, except for the
+          -- top-level scope which will be rendered as a scope.
+          enabled = true,
+          -- only show chunk scopes in the current window
+          only_current = false,
+          priority = 200,
+          hl = "SnacksIndentChunk", ---@type string|string[] hl group for chunk scopes
+          char = {
+            -- corner_top = "┌",
+            -- corner_bottom = "└",
+            corner_top = "╭",
+            corner_bottom = "╰",
+            horizontal = "─",
+            vertical = "│",
+            arrow = ">",
+          },
+        },
+      },
       input = { enabled = true },
       picker = {
         enabled = true,
