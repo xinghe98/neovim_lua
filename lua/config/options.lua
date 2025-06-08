ea4339c023d29378de91460f3e84bfff6a968268
xinghe98 <lcg9899@gmail.com>
2025-06-08 11:53:51 +0800

    up

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -45,6 +45,16 @@ vim.o.foldmethod = "indent"
 vim.o.foldenable = false
 vim.o.foldlevel = 99
 vim.opt.clipboard = "unnamedplus"
+if vim.fn.has("wsl") == 1 then
+  vim.api.nvim_create_autocmd("TextYankPost", {
+
+    group = vim.api.nvim_create_augroup("Yank", { clear = true }),
+
+    callback = function()
+      vim.fn.system("clip.exe", vim.fn.getreg('"'))
+    end,
+  })
+end
 vim.opt.pumblend = 0
 vim.opt.winblend = 0
 -- translate
